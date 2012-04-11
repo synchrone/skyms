efcfd21fa6142c5f81eb32d392334c77ae5b6439
Alexander Bogdanov <syn@li.ru>
2012-04-11 19:36:54 +0400

    A working dotCloud setup

--- skyms/settings.py
+++ skyms/settings.py
@@ -1,5 +1,5 @@
 #skype interaction client cert
-keyFileName = 'dev.pem'
+keyFileName = '/home/dotcloud/current/dev.pem'
 #the account data
 skypeLogin = 'username'
 skypePassword = 'password'
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append('/home/dotcloud/current')
+from skyms import app as flask_app
+application = flask_app.app
